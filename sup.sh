42e70a6181b12cf7be0a442d8e7f992f28373a58
kpberry <kpberry11@gmail.com>
2017-06-09 18:44:43 -0400

    sup

---
+++ sup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sup_son="¯\\_(ツ)_/¯"
+sup_sons=1
+delay=${1:-0}
+
+
+while [ $[($sup_sons + 1) * (1 + ${#sup_son})] -le $(tput cols) ]; do
+        j=$[ ${#sup_son} - 1]
+        m="" 
+        while [ "$j" -ge 0 ]; do
+                sleep $delay
+                m="${sup_son:$j:1}$m"
+                printf "$m\r" 
+                j=$[j - 1]
+        done
+
+        j=0
+        while [ "$j" -le $[$(tput cols) - (${#sup_son} + 1) * $sup_sons - $sup_sons] ]; do
+                sleep $delay
+                m=" $m"
+                printf "$m\r"
+                j=$[j + 1]
+        done
+        sup_sons=$[sup_sons + 1]
+done
+
+printf "\n\r"
